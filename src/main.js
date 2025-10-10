77e5f635716293c4665ebb4460dbdada550b17b9
Aninhasanchez <analuisasanchez05@gmail.com>
2025-10-10 13:56:43 -0300

    10-10

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const btn = document.getElementById("btn-simular");
 function registrarPresenca(nome) {
   const horario = new Date().toLocaleTimeString();
   const linha = document.createElement("tr");
-  linha.className = "hover:bg-gray-50 transition";
+  linha.className = "hover:bg-purple-50 transition";
   linha.innerHTML = `
     <td class="p-4 font-medium">${nome}</td>
     <td class="p-4 text-green-600 font-semibold">Presente</td>
